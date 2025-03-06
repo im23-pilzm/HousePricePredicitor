a11c48b94179ad41787a64c326f440cadda12080
im23-pilzm <pilzm@bzz.ch>
2025-03-06 16:27:28 +0100

    Read a Dataset with Pandas

--- src/data-preprocessing.py
+++ src/data-preprocessing.py
@@ -1,2 +1,12 @@
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.expand_frame_repr', False)
+pd.set_option('display.width', 200)
+
+df = pd.read_csv("../dataset/house-price/test.csv")
+
+print(df.columns.tolist())
+
+print(df.dtypes.T)
