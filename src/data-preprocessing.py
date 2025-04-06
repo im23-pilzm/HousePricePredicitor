0a05ee84dde53eb50beda4a4559ebddb9469a1ab
im23-pilzm <pilzm@bzz.ch>
2025-04-06 21:51:11 +0200

    Changing values where NA makes sense to "None" for better processing. Creating lists for attributes where NA makes no sens.

--- src/data-preprocessing.py
+++ src/data-preprocessing.py
@@ -1,54 +1,75 @@
+from statistics import median
+
 import pandas as pd
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.impute import SimpleImputer
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.expand_frame_repr', False)
 pd.set_option('display.width', 200)
 
-df = pd.read_csv("../dataset/house-price/raw-data/test.csv")
 
-if df.empty:
-    print("DataFrame is empty!")
-else:
+def check_missing_values(df):
+    if df.empty:
+        print("DataFrame is empty.")
+        return
+
+    missing_per_col = df.isnull().sum()
+    total_missing = missing_per_col.sum()
+
+    if total_missing == 0:
+        print("No missing values found.")
+        return
+
+    print(f"Found {total_missing} missing values in DF.")
+
+    missing_columns = missing_per_col[missing_per_col > 0]
+    print(missing_columns)
+
+    missing_rows = df[df.isnull().any(axis=1)]
+    print(missing_rows)
 
 
+house_df = pd.read_csv("../dataset/house-price/raw-data/test.csv")
 
+none_cols = ['Alley', 'Fence', 'FireplaceQu', 'GarageType', 'GarageFinish',
+             'GarageQual', 'GarageCond', 'BsmtQual', 'BsmtCond', 'BsmtExposure',
+             'BsmtFinType1', 'BsmtFinType2', 'MasVnrType', 'PoolQC', 'MiscFeature']
 
+house_df[none_cols] = house_df[none_cols].fillna("None")
 
-
-
-
-
-
-
-
-#print(df.info())
-
-#drop rows with missing data
-#df.isnull().sum()
-#df.dropna(inplace=True)
-
-#remove duplicates
-#df.duplicated().sum()
-#df.drop_duplicates(inplace=True)
-
-# List of columns to scale
-columns_to_scale = [
-    "LotFrontage", "LotArea", "YearBuilt", "YearRemodAdd", "MasVnrArea",
-    "BsmtFinSF1", "BsmtFinSF2", "BsmtUnfSF", "TotalBsmtSF", "1stFlrSF",
-    "2ndFlrSF", "GrLivArea", "GarageArea", "WoodDeckSF", "OpenPorchSF",
-    "EnclosedPorch", "3SsnPorch", "ScreenPorch", "PoolArea", "MiscVal"
+mode_columns = [
+    "MSZoning",
+    "Street",
+    "LotShape",
+    "LandContour",
+    "Utilities",
+    "LotConfig",
+    "LandSlope",
+    "Neighborhood",
+    "Condition1",
+    "Condition2",
+    "BldgType",
+    "HouseStyle",
+    "RoofStyle",
+    "RoofMatl",
+    "Exterior1st",
+    "Exterior2nd",
+    "ExterQual",
+    "ExterCond",
+    "Foundation",
+    "Heating",
+    "HeatingQC",
+    "CentralAir",
+    "Electrical",
+    "KitchenQual",
+    "Functional",
+    "PavedDrive",
+    "SaleType",
+    "SaleCondition"
 ]
 
-#standardize & normalize data
-#st_scaler = MinMaxScaler()
-#df[columns_to_scale] = st_scaler.fit_transform(df[columns_to_scale])
-#no_scaler = StandardScaler()
-#df[columns_to_scale] = no_scaler.fit_transform(df[columns_to_scale])
-#df.dropna(inplace=True)
-#if df.empty:
-    #print("Error: DataFrame is empty after dropping NaN values.")
-    #exit()  # Stop execution to avoid further errors
+#TODO create a function that checks if a garage exists
 
-#df.info()
-#df.describe()
+
+check_missing_values(house_df)
