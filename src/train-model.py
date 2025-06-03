eeb20043ae8dc8abd7055cce96fa8c3f300d5c31
im23-pilzm <pilzm@bzz.ch>
2025-06-03 15:00:52 +0200

    Implemented train-model.py.

--- src/train-model.py
+++ src/train-model.py
@@ -3,3 +3,23 @@ import pickle
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error
+
+df = pd.read_csv('../dataset/house-price/processed-data/train_processed.csv')
+
+x = df.drop(columns=["SalePrice"])
+y = df["SalePrice"]
+
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_val)
+
+mae = mean_absolute_error(y_val, y_pred)
+print(f"Validation MAE: {mae:.2f}")
+
+with open('../models/linear_regression_model.pkl', 'wb') as f:
+    pickle.dump(model, f)
+
+print("Model trained and saved.")
